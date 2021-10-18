ddcfdf97e58bc7f5ebe9aab3dd3cdaf23e14671d
Vinicius Freitas <vinfre@on.sinch.com>
2021-10-18 09:10:49 -0300

    Added web

--- web/script/index.js
+++ web/script/index.js
@@ -1,31 +1,29 @@
 const btnSend = document.querySelector('.btn-send');
-
-const stage = document.getElementById('stage').value;
-
-const bot = parseInt(document.getElementById('bot').value);
-
-const userNumber = document.getElementById('userNumber').value;
-
 const text = document.getElementsByClassName('text-response')[0]
 
-btnSend.addEventListener("click", function (e) {
+
+btnSend.addEventListener("click", async function (e) {
     e.preventDefault();
 
-    sendData()
+
+    const stage = document.getElementById('stage').value;
+
+    const bot = parseInt(document.getElementById('bot').value);
+
+    const userNumber = document.getElementsByClassName('userNumber')[0].value;
+
+    await sendData(stage, bot, userNumber)
 
 });
 
-function sendData(){
-    const response = fetch('https://naturasessao.herokuapp.com/reset', {
-        method: 'POST',
-        body: {
-            stage,
-            bot,
-            userNumber
-        }
-    }).then(
-        text.removeAttribute('hidden')
-    ).catch(
-        text.removeAttribute('hidden')
-    );
+async function sendData(stage, bot, userNumber) {
+   
+    axios.post('http://localhost:3333/reset', {
+        stage,
+        bot,
+        userNumber
+    }).then(res => {
+        console.log(res)
+    });
+
 }
