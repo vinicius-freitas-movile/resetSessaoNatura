4bd56a5b2044c11553ecdaeb1d5bd4166c89570d
Vinicius Freitas <vinfre@on.sinch.com>
2021-11-01 11:17:50 -0300

    added toast and refator css

--- reset-session-web/src/js/App.js
+++ reset-session-web/src/js/App.js
@@ -1,48 +1,45 @@
 import '../css/App.css';
 import LogoSinch from '../img/LogoSinch.png';
 import IconHome from '../img/iconHome.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { InputGroup, Tooltip, OverlayTrigger, Button } from 'react-bootstrap'
 import { FaQuestionCircle } from 'react-icons/fa';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
- //ta quebrando essa bgaça ver o pq não está alterando o estado do check
+  //ta quebrando essa bgaça ver o pq não está alterando o estado do check
   const [stage, setStage] = useState('DRAFT');
-  const [bot, setBot] = useState(3052);
+  const [bot, setBot] = useState('3052');
   const [userNumber, setUserNumber] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(stage, bot)
-  }
+    if (!isNaN(Number(userNumber))) {
+      deleteSession();
 
-  const deleteSession = () => {
-
-  }
-
-  const changeRadioButton = (event) => {
-
-    let data = event.target.value;
-    let convertInNumber = Number(data);
-
-    if (!isNaN(convertInNumber)) {
-      setBot(bot, convertInNumber);
-      console.log(bot)
-    } else {
-      setStage(data);
-      console.log(stage)
+      showToast();
     }
   }
 
-
-  const changeBot = (event) => {
-
+  const showToast = () => {
+    toast.success("Sessão resetada com sucesso!", {
+      position: toast.POSITION.BOTTOM_RIGHT
+    });
   }
 
-  const renderTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props} className="tooltipText">
+  const deleteSession = async () => {
+    await axios.post('https://naturasessao.herokuapp.com/reset', {
+      stage,
+      bot: Number(bot),
+      userNumber
+    });
+  }
+
+  const renderTooltip = () => (
+    <Tooltip id="button-tooltip" className="tooltipText">
       <p>
         Formato aceito:<br />
         DDI + DDD + Numero<br />
@@ -69,47 +66,34 @@ function App() {
         <div className="bodyReset">
           <div className="headerBodyReset">
             <h1>Reset de Sessão</h1>
-            <p>Resete a sua sessão em um de nossos bots</p>
           </div>
           <section className="infoReset">
             <form onSubmit={handleSubmit}>
               <div className="container">
                 <div className="headerInfoReset">
                   <p className="PBold">Ambiente</p>
-                  <p>Selecione entre homologação e produção</p>
                 </div>
                 <InputGroup className="mb-3 radioGroup">
-                  <InputGroup.Radio aria-label="DRAFT" name="radioStage" id="radioHomol dev" value="DRAFT" onChange={changeRadioButton} checked={stage === 'DRAFT'} />
+                  <InputGroup.Radio aria-label="DRAFT" name="radioStage" id="radioHomol dev" value="DRAFT" onChange={(e) => { setStage(e.target.value) }} checked={stage === 'DRAFT'} />
                   <InputGroup.Text id="radioHomol">Homologação</InputGroup.Text>
-                  <InputGroup.Radio aria-label="LIVE" name="radioStage" value="LIVE" id="live" onChange={changeRadioButton} checked={stage === 'LIVE'} />
+                  <InputGroup.Radio aria-label="LIVE" name="radioStage" id="radioHomol" value="LIVE" id="live" onChange={(e) => { setStage(e.target.value) }} checked={stage === 'LIVE'} />
                   <InputGroup.Text>Produção</InputGroup.Text>
                 </InputGroup>
                 <p className="PBold">Bot</p>
-                <p>Escolha um dos bots abaixo</p>
                 <InputGroup className="mb-3 radioGroup">
-                  <InputGroup.Radio aria-label="Avon Hisp" name="radioBot" value={3242} onChange={changeRadioButton} checked={bot === 3242} />
+                  <InputGroup.Radio aria-label="Avon Hisp" name="radioBot" value='3242' onChange={(e) => { setBot(e.target.value) }} checked={bot === '3242'} />
                   <InputGroup.Text id="radioHomol">Avon Hispânica</InputGroup.Text>
-                  <InputGroup.Radio aria-label="Nat BR" name="radioBot" value={3052} onChange={changeRadioButton} checked={bot === 3052} />
+                  <InputGroup.Radio aria-label="Nat BR" name="radioBot" value='3052' onChange={(e) => { setBot(e.target.value) }} checked={bot === '3052'} />
                   <InputGroup.Text>Natura Brasil</InputGroup.Text>
-                  <InputGroup.Radio aria-label="Nat Latam" name="radioBot" value={3859} onChange={changeRadioButton} checked={bot === 3859} />
+                  <InputGroup.Radio aria-label="Nat Latam" name="radioBot" value='3859' onChange={(e) => { setBot(e.target.value) }} checked={bot === '3859'} />
                   <InputGroup.Text>Natura Hispânica</InputGroup.Text>
                 </InputGroup>
                 <p className="PBold">Número</p>
-                <div>
-                  <p>Número que terá a sessão resetada
-                    <OverlayTrigger
-                      placement="right"
-                      delay={{ show: 250, hide: 400 }}
-                      overlay={renderTooltip}
-                    >
-                      <Button className="tooltipButton" variant="none"><FaQuestionCircle className="IconTooltip" /></Button>
-                    </OverlayTrigger>
-                  </p>
-                </div>
                 <div className="footerReset">
-                  <input type="text" placeholder="5511988589971" className="userNumber"></input>
+                  <input type="text" required="required" placeholder="5511988589971" className="userNumber" onChange={(e) => { setUserNumber(e.target.value) }}></input>
                   <div className="footerButton">
-                    <Button className="buttonReset" variant="success" type="submit">Resetar</Button>
+                    <button className="buttonReset" className="btn btn-success" type="submit">Resetar</button>
+                    <ToastContainer />
                   </div>
                 </div>
               </div>
